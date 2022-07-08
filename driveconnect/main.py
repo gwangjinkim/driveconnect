6e2c1e3985ca98b6e823be65f0c5eac41aee4fc6
Kim <KIMGW1@novartis.net>
2022-07-08 18:42:05 +0200

    correct strips

--- driveconnect/main.py
+++ driveconnect/main.py
@@ -25,33 +25,40 @@ def get_available_drives():
                              bin(drive_bitmask)[:1:-1])))
 
 def is_drive_connected(drive_letter):
-    return drive_letter.rstrip(':') in get_available_drives()
+    drive_letter = drive_letter.rstrip(':')
+    return drive_letter in get_available_drives()
 
 # https://docs.microsoft.com/en-us/previous-versions/windows/it-pro/windows-server-2012-r2-and-2012/gg651155(v=ws.11)
 # https://discuss.dizzycoding.com/what-is-the-best-way-to-map-windows-drives-using-python/
 # the first possibility
 
 def _connect_drive(drive_letter, server_path, username, password):
-    cmd_ = f"net use {drive_letter.rstrip(':')}: \\\\{server_path.lstrip('\\')} /user:{username} {password}"
+    drive_letter = drive_letter.rstrip(':')
+    server_path = server_path.lstrip('\\')
+    cmd_ = f"net use {drive_letter}: \\\\{server_path} /user:{username} {password}"
     # print(cmd_)
     return cmd(cmd_)
 
 def _disconnect_drive(drive_letter):
-    cmd_ = f"net use {drive_letter.rstrip(':')}: /del"
+    drive_letter = drive_letter.rstrip(':')
+    cmd_ = f"net use {drive_letter}: /del"
     # print(cmd_)
     return cmd(cmd_)
 
 def connect_drive(drive_letter, server_path, user_file=None, pass_file=None):
+    drive_letter = drive_letter.rstrip(':')
+    server_path = server_path.lstrip('\\')
     if not is_drive_connected(drive_letter):
         if user_file is None and pass_file is None:
-            cmd_ = f"net use {drive_letter}: \\\\{server_path.lstrip('\\')}"
+            cmd_ = f"net use {drive_letter}: \\\\{server_path}"
             # print(cmd_)
             cmd(cmd_)
         else:
             co = pss.get_credentials(user_file, pass_file)
             _connect_drive(drive_letter, server_path, co.username, co.password)
-        print(f"{server_path} successfully connected to {drive_letter.rstrip(':')}:")
+        print(f"{server_path} successfully connected to {drive_letter}:")
 
 def disconnect_drive(drive_letter):
-    if is_drive_connected(drive_letter.rstrip(':')):
-        _disconnect_drive(drive_letter.rstrip(':'))
+    drive_letter = drive_letter.rstrip(':')
+    if is_drive_connected(drive_letter):
+        _disconnect_drive(drive_letter)
